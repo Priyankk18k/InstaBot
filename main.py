35e0316f7704805199f697533d497a2ac7bc8ad1
Priyank <35269717+Priyankk18k@users.noreply.github.com>
2018-02-15 20:27:31 +0530

    added new functionality
    added new functionality

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import requests
+import requests,urllib
 
 
-APP_ACCESS_TOKEN = 'f3879afc4df64c588581bcda0f1ddec0'
+APP_ACCESS_TOKEN = '4870715640.a48e759.874aba351e5147eca8a9d36b9688f494'
 BASE_URL='https://api.instagram.com/v1/'
 
 def self_info():
@@ -10,16 +10,109 @@ def self_info():
     user_info = requests.get(request_url).json()
 
     if user_info['meta']['code'] == 200:
-        print user_info['data']['username']
-        print user_info['data']['bio']
-        print user_info['data']['follows']
-        print user_info['data']['followed_by']
-        print user_info['data']['website']
-        print user_info['data']['id']
-        print user_info['data']['full_name']
-        print user_info['data']['profile_picture']
+        print 'Username: %s' %(user_info['data']['username'])
+        print'Bio: %s' %(user_info['data']['bio'])
+        print 'No. of people you follow: %s' %(user_info['data']['counts']['follows'])
+        print 'No. of followers: %s' %(user_info['data']['counts']['followed_by'])
+        print 'Your website : %s' %(user_info['data']['website'])
+        print 'Your id: %s' %(user_info['data']['id'])
+        print 'Your full name : %s'%(user_info['data']['full_name'])
+        print 'Your Profle picture is %s'%(user_info['data']['profile_picture'])
     else:
         print 'Status code other than 200 received!'
 
 
-self_info()
+#self_info()
+def get_own_post():
+    request_url = (BASE_URL + 'users/self/media/recent/?access_token=%s') % (APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    own_media = requests.get(request_url).json()
+
+    if own_media['meta']['code'] == 200:
+        if len(own_media['data']):
+            image_name = own_media['data'][0]['id'] + '.jpeg'
+            image_url = own_media['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url, image_name)
+            print 'Your image has been downloaded!'
+        else:
+            print 'Post does not exist!'
+    else:
+        print 'Status code other than 200 received!'
+
+def get_user_id(insta_username):
+    request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if len(user_info['data']):
+            return user_info['data'][0]['id']
+        else:
+            return None
+    else:
+        print 'Status code other than 200 received!'
+        exit()
+
+def get_user_info(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_info = requests.get(request_url).json()
+
+    if user_info['meta']['code'] == 200:
+        if 'data' in user_info:
+            print 'Username: %s' % (user_info['data']['username'])
+            print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
+            print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
+            print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+        else:
+            print 'There is no data for this user!'
+    else:
+        print 'Status code other than 200 received!'
+def get_user_post(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print 'User does not exist!'
+        exit()
+    request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+    print 'GET request url : %s' % (request_url)
+    user_media = requests.get(request_url).json()
+    if len(user_media['data']):
+            image_name = user_media['data'][0]['id'] + '.jpeg'
+            image_url = user_media['data'][0]['images']['standard_resolution']['url']
+            urllib.urlretrieve(image_url, image_name)
+            print 'Your image has been downloaded!'
+    else:
+        print 'Post does not exist!'
+
+def start_bot():
+    while True:
+        print '\n'
+        print 'Hey! Welcome to instaBot!'
+        print 'Here are your menu options:'
+        print "a.Get your own details\n"
+        print "b.Get details of a user by username\n"
+        print "c.Get your own recent post\n"
+        print "d.Get the recent post of a user by username\n"
+        print "j.Exit"
+
+        choice = raw_input("Enter you choice: ")
+        if choice == "a":
+            self_info()
+        elif choice == "b":
+            insta_username = raw_input("Enter the username of the user: ")
+            get_user_info(insta_username)
+        elif choice == "c":
+            get_own_post()
+        elif choice == "d":
+            insta_username = raw_input("Enter the username of the user: ")
+            get_user_post(insta_username)
+        elif choice == "j":
+            exit()
+        else:
+            print "wrong choice"
+
+start_bot()
